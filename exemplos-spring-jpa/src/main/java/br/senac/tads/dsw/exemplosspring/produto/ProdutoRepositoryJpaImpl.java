c88caf7e41cce720cac55641ec07578f30141fae
Fernando Tsuda <fernando@tvod.com.br>
2019-10-15 16:00:06 -0300

    Exemplos com Criteria API e EntityGraph

--- exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspring/produto/ProdutoRepositoryJpaImpl.java
+++ exemplos-spring-jpa/src/main/java/br/senac/tads/dsw/exemplosspring/produto/ProdutoRepositoryJpaImpl.java
@@ -2,9 +2,16 @@ package br.senac.tads.dsw.exemplosspring.produto;
 
 import java.util.List;
 
+import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 public class ProdutoRepositoryJpaImpl implements ProdutoRepository {
@@ -19,10 +26,32 @@ public class ProdutoRepositoryJpaImpl implements ProdutoRepository {
 		return jpqlQuery.getResultList();
 	}
 
+	// Exemplos Criteria API: https://www.objectdb.com/java/jpa/query/criteria#Parameters_in_Criteria_Queries
+	public List<Produto> findAllCriteria(int offset, int quantidade) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = cb.createQuery(Produto.class);
+		criteriaQuery.from(Produto.class);
+		TypedQuery<Produto> query = em.createQuery(criteriaQuery)
+				.setFirstResult(offset).setMaxResults(quantidade);
+		return query.getResultList();
+	}
+
 	@Override
 	public List<Produto> findByCategoria(List<Integer> idsCat, int offset, int quantidade) {
 		Query jpqlQuery = em.createNamedQuery("Produto.findAllByCategorias_Id").setParameter("idCat", idsCat);
 		return jpqlQuery.getResultList();
+	}
+	
+	public List<Produto> findByCategoriaCriteria(List<Integer> idsCat, int offset, int quantidade) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = cb.createQuery(Produto.class);
+		Root<Produto> root = criteriaQuery.from(Produto.class);
+		Expression<Integer> categoriaExpression = root.get("Produto.categorias");
+		Predicate categoriaPredicate = categoriaExpression.in(idsCat);
+		criteriaQuery.where(categoriaPredicate);
+		TypedQuery<Produto> query = em.createQuery(criteriaQuery)
+				.setFirstResult(offset).setMaxResults(quantidade);
+		return query.getResultList();
 	}
 
 	@Override
@@ -30,6 +59,30 @@ public class ProdutoRepositoryJpaImpl implements ProdutoRepository {
 		Query jpqlQuery = em.createNamedQuery("Produto.findById").setParameter("idProd", id);
 		Produto p = (Produto) jpqlQuery.getSingleResult();
 		return p;
+	}
+	
+	public Produto findByIdCriteria(Long id) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = cb.createQuery(Produto.class);
+		Root<Produto> root = criteriaQuery.from(Produto.class);
+		criteriaQuery.where(cb.equal(root.get("id"), id));
+		TypedQuery<Produto> query = em.createQuery(criteriaQuery);
+		return query.getSingleResult();
+	}
+
+	// Exemplos entityGraph
+	// - https://www.baeldung.com/jpa-entity-graph
+	// - https://thoughts-on-java.org/jpa-21-entity-graph-part-2-define/
+	// - https://docs.oracle.com/javaee/7/tutorial/persistence-entitygraphs001.htm
+	// javax.persistence.fetchgraph --> Campos que serão retornados devem estar explicitos
+	// javax.persistence.loadgraph --> Campos que serão retornados são adicionais aos campos padrao definidos no modelo
+	public Produto findByIdEntityGraph(Long id) {
+		EntityGraph<Produto> entityGraph = em.createEntityGraph(Produto.class);
+		entityGraph.addAttributeNodes("categorias", "imagens");
+
+		Query jpqlQuery = em.createQuery("SELECT p FROM Produto p WHERE p.id = :idProd")
+				.setParameter("idProd", id).setHint("javax.persistence.loadgraph", entityGraph);
+		return (Produto) jpqlQuery.getSingleResult();
 	}
 
 	@Override
