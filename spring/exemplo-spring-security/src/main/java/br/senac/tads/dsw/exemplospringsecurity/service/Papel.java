51ae61e7cb68828910da6308f8affad3455fabb6
Fernando Tsuda <ftsuda.senac@gmail.com>
2019-11-14 22:26:24 -0300

    Reorganização dos projetos e inclusão de exemplos

---
+++ spring/exemplo-spring-security/src/main/java/br/senac/tads/dsw/exemplospringsecurity/service/Papel.java
@@ -0,0 +1,33 @@
+package br.senac.tads.dsw.exemplospringsecurity.service;
+
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ *
+ * @author fernando.tsuda
+ */
+public class Papel implements GrantedAuthority {
+
+    private String nome;
+
+    public Papel() {
+    }
+
+    public Papel(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+	@Override
+	public String getAuthority() {
+		return nome;
+	}
+
+}
