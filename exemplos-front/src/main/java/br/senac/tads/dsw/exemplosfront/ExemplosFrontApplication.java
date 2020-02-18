d449845bb446c53407fb5ec2613432a18a4a00ad
fernando.tsuda <fernando.tsuda@CAS419218W1001.senacsp.edu.br>
2020-02-17 22:26:37 -0300

    Exemplos diversos e projeto base Servlet/JSP

---
+++ exemplos-front/src/main/java/br/senac/tads/dsw/exemplosfront/ExemplosFrontApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplosfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplosFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplosFrontApplication.class, args);
+	}
+
+}
---
+++ exemplos-front/src/test/java/br/senac/tads/dsw/exemplosfront/ExemplosFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.dsw.exemplosfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
