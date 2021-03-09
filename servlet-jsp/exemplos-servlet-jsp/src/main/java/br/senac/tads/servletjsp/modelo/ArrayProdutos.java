30b0736232323c135359d31785901ac44cdc4cbf
ftsuda-senac <ftsuda.senac@gmail.com>
2021-03-09 10:42:34 -0300

    Adicionando exemplo jsp:useBean

---
+++ servlet-jsp/exemplos-servlet-jsp/src/main/java/br/senac/tads/servletjsp/modelo/ArrayProdutos.java
@@ -0,0 +1,23 @@
+package br.senac.tads.servletjsp.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Wrapper para permitir manter produtos em uma ArrayList no jsp quando usa jsp:useBean
+public class ArrayProdutos {
+
+  private List<Produto> produtos = new ArrayList<>();
+
+  public List<Produto> getProdutos() {
+    return produtos;
+  }
+
+  public void setProdutos(List<Produto> produtos) {
+    this.produtos = produtos;
+  }
+
+  public void setProduto(Produto p) {
+    this.produtos.add(p);
+  }
+
+}
