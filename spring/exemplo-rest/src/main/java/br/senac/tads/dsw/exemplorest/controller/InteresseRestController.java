14ae38939a3135fcb43a0608fc7622427d25b874
ftsuda-senac <ftsuda.senac@gmail.com>
2022-05-25 14:53:16 -0300

    Exemplo REST novo 2022-1

--- spring/exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/InteresseRestController.java
+++ spring/exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/InteresseRestController.java
@@ -1,25 +1,25 @@
 package br.senac.tads.dsw.exemplorest.controller;
 
+import br.senac.tads.dsw.exemplorest.dominio.Interesse;
+import br.senac.tads.dsw.exemplorest.dominio.InteresseRepository;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import br.senac.tads.dsw.exemplorest.dominio.Interesse;
-import br.senac.tads.dsw.exemplorest.dominio.InteresseRepository;
 
 @RestController
 @RequestMapping("/rest/interesses")
-public class InteressesController {
-
+public class InteresseRestController {
+    
     private InteresseRepository interesseRepository;
-
-    public InteressesController(InteresseRepository interesseRepository) {
+    
+    public InteresseRestController(InteresseRepository interesseRepository) {
         this.interesseRepository = interesseRepository;
     }
-
+    
     @GetMapping
     public List<Interesse> findAll() {
         return interesseRepository.findAll();
     }
-
+    
 }
