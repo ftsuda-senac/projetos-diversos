7273b67c53839331477ea6360f5a74a30166db2d
ftsuda-senac <ftsuda.senac@gmail.com>
2022-06-07 11:01:16 -0300

    Ajustes nos exemplos frontend básicos

--- exemplos-front/src/main/java/br/senac/tads/dsw/exemplosfront/ExemplosFrontApplication.java
+++ exemplos-front/src/main/java/br/senac/tads/dsw/exemplosfront/ExemplosFrontApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExemplosFrontApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExemplosFrontApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExemplosFrontApplication.class, args);
+    }
 
 }
