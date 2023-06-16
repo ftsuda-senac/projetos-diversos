de73fdd5da479f842b9d9f5903c17a4aa18f212c
ftsuda-senac <ftsuda.senac@gmail.com>
2023-06-16 20:23:34 -0300

    Versão 2 para renomear arquivo txt

--- file-renamer/src/main/java/br/senac/tads/filerenamer/FileRenamerV2.java
+++ file-renamer/src/main/java/br/senac/tads/filerenamer/FileRenamerV2.java
@@ -8,11 +8,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.text.MessageFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-public class FileRenamer {
+public class FileRenamerV2 {
 
     public static void main(String[] args) {
 //        if (args == null || args.length == 0) {
@@ -28,7 +31,7 @@ public class FileRenamer {
 //        }
 //        String directory = args[0];
 
-        String directory = "C:\\senac\\2023-1\\dswb-atividade-musica";
+        String directory = "C:\\senac\\2023-1\\dswa-atividade-filme";
         Path directoryPath = Paths.get(directory);
 
         if (!Files.exists(directoryPath)) {
@@ -46,8 +49,9 @@ public class FileRenamer {
         System.out.println(MessageFormat.format("Renomeando arquivos do caminho \"{0}\"...", directory));
         Pattern patt = Pattern.compile("Nome: (.+) \\(");
 
-        try ( Stream<Path> paths = Files.walk(Paths.get(directory))) {
+        Map<String, String> mapFileAndName = new LinkedHashMap<>();
 
+        try (Stream<Path> paths = Files.walk(Paths.get(directory))) {
             paths.filter(p1 -> Files.isRegularFile(p1) && p1.getFileName().toString().endsWith(".txt"))
                     .forEach(p1 -> {
 
@@ -55,42 +59,42 @@ public class FileRenamer {
                         String originalFileName = originalFile.toString().split("\\.")[0];
                         // System.out.println(originalFileName);
                         String personName = null;
-                        try ( BufferedReader br = new BufferedReader(new FileReader(p1.toFile()))) {
+                        try (BufferedReader br = new BufferedReader(new FileReader(p1.toFile()))) {
                             String st;
                             while ((st = br.readLine()) != null) {
                                 Matcher matcher = patt.matcher(st);
                                 if (matcher.find() && matcher.groupCount() > 0) {
                                     personName = matcher.group(1);
+                                    mapFileAndName.put(originalFileName, personName);
                                     break;
                                 }
                             }
                         } catch (IOException ex) {
                             ex.printStackTrace();
                         }
-                        if (personName != null) {
-                            // System.out.println(personName);
-                            final String personName2 = personName;
-                            try ( Stream<Path> paths2 = Files.walk(Paths.get(directory))) {
-                                paths2.filter(p2 -> Files.isRegularFile(p2) && !(p2.getFileName().toString().endsWith(".txt")) && p2.getName(p2.getNameCount() - 1).toString().startsWith(originalFileName))
-                                        .forEach(p2 -> {
-                                            String fileExtension = p2.getFileName().toString().split("\\.")[1].toLowerCase();
-                                            Path newPath = Paths.get(directory, personName2.replaceAll("\\s+", "-") + "." + fileExtension);
-                                            //System.out.println(newPath.toString());
-                                            try {
-                                                Files.move(p2, newPath, StandardCopyOption.REPLACE_EXISTING);
-                                            } catch (IOException ex) {
-                                                ex.printStackTrace();
-                                            }
-                                        });
-                            } catch (IOException ex) {
-                                ex.printStackTrace();
-                            }
-                        }
                     });
-
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+
+        for (Entry<String, String> entry : mapFileAndName.entrySet()) {
+            try (Stream<Path> paths2 = Files.walk(Paths.get(directory))) {
+                paths2.filter(p2 -> Files.isRegularFile(p2) && p2.getName(p2.getNameCount() - 1).toString().startsWith(entry.getKey()))
+                        .forEach(p2 -> {
+                            String fileExtension = p2.getFileName().toString().split("\\.")[1].toLowerCase();
+                            Path newPath = Paths.get(directory, entry.getValue().replaceAll("\\s+", "-") + "." + fileExtension);
+                            //System.out.println(newPath.toString());
+                            try {
+                                Files.move(p2, newPath, StandardCopyOption.REPLACE_EXISTING);
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
+                        });
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         System.out.println("Feito");
     }
 
